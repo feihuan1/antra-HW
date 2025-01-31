1a7e07b6a231cea0e252c411abc1c4dc94a9bab4
Feihuan Peng <ericrealtor1997@gmail.com>
2025-01-31 15:52:08 -0800

    add timer

--- evaluation/script.js
+++ evaluation/script.js
@@ -1,18 +1,19 @@
-
 const GameModel = {
   isGameOver: false,
   score: 0,
   level: 1,
-  updateScore: function(points) {
+  time: 30,
+  updateScore: function (points) {
     if (this.isGameOver) return;
     this.score += points;
     this.level = Math.floor(this.score / 100 + 1);
   },
-  resetGame: function() {
+  resetGame: function () {
     this.isGameOver = false;
     this.score = 0;
     this.level = 1;
-  }
+    this.time = 30;
+  },
 };
 
 const GameView = {
@@ -22,74 +23,93 @@ const GameView = {
   pop: document.querySelector(".pop"),
   enemy: document.querySelector("#enemy"),
   player: document.getElementById("player"),
-  start:document.getElementById("start"),
-  
-  updateUI: function(e) {
+  start: document.getElementById("start"),
+
+  updateUI: function (e) {
     document.getElementById("level").innerText = GameModel.level;
     document.getElementById("score").innerText = GameModel.score;
-    e.target.style.transform = "scale(1.5 , 0.3)";
-    e.target.style.transformOrigin = "bottom";
+    document.getElementById("time").innerText = GameModel.time;
+    if (e) {
+      e.target.style.transform = "scale(1.5 , 0.3)";
+      e.target.style.transformOrigin = "bottom";
+    }
   },
-  playSound: function(path, vol) {
+  playSound: function (path, vol) {
     let sound = new Audio(path);
     sound.volume = vol;
     sound.play();
   },
-  updateCursor: function(down) {
-    this.board.style.cursor = `url('./image/${down ? "hammerdown" : "hammerup"}.png'), auto`;
+  updateCursor: function (down) {
+    this.board.style.cursor = `url('./image/${
+      down ? "hammerdown" : "hammerup"
+    }.png'), auto`;
   },
-  showGameOver: function() {
+  showGameOver: function () {
     this.pop.classList.remove("hidden");
     this.finalScore.innerText = GameModel.score;
     this.finalScore.style.color = GameModel.score < 100 ? "red" : "green";
     this.music.pause();
   },
-  renderTile: function(id) {
+  renderTile: function (id) {
     let tile = document.createElement("div");
     tile.id = id;
     this.board.appendChild(tile);
   },
-  renderEntity: function(type, tile) {
+  renderEntity: function (type, tile) {
     let entity = document.createElement("img");
     entity.id = type;
-    if(type === "enemy"){
-      entity.src = GameView.enemy.files[0] ? URL.createObjectURL(enemy.files[0]) : "./image/feihuan1.png"
+    if (type === "enemy") {
+      entity.src = GameView.enemy.files[0]
+        ? URL.createObjectURL(enemy.files[0])
+        : "./image/feihuan1.png";
     } else {
-      entity.src = GameView.player.files[0] ? URL.createObjectURL(player.files[0]) : "./image/vita.png"
+      entity.src = GameView.player.files[0]
+        ? URL.createObjectURL(player.files[0])
+        : "./image/vita.png";
     }
     entity.draggable = false;
     tile.appendChild(entity);
-  }
+  },
 };
 
 const GameController = {
-  startGame: function() {
+  startGame: function () {
     GameView.music.play();
     GameView.start.remove();
     GameController.init();
-
   },
-  init: function() {
+  init: function () {
     for (let i = 0; i < 16; i++) {
       GameView.renderTile(i.toString());
     }
-    document.getElementById("board").addEventListener("click", this.handleTileClick);
+    let timerInterval = setInterval(() => {
+      if (GameModel.time <= 1) {
+        GameModel.isGameOver = true;
+        clearInterval(timerInterval);
+        GameView.showGameOver();
+      }
+      GameModel.time--;
+      GameView.updateUI();
+    }, 1000);
+
+    document
+      .getElementById("board")
+      .addEventListener("click", this.handleTileClick);
     this.startGameLoops();
   },
-  handleTileClick: function(e) {
+  handleTileClick: function (e) {
     if (GameModel.isGameOver) return;
     GameView.updateCursor(true);
     setTimeout(() => GameView.updateCursor(false), 50);
-    
+
     if (e.target.id === "enemy") {
       GameView.playSound("./audio/feihuanhit.mp3", 0.5);
       GameView.playSound("./audio/hammerhead.mp3", 0.5);
       GameModel.updateScore(10);
       GameView.updateUI(e);
-      setTimeout(()=>{
+      setTimeout(() => {
         e.target.remove();
-      }, 1000)
-
+      }, 1000);
     } else if (e.target.id === "player") {
       GameView.playSound("./audio/vitahit.mp3", 0.5);
       GameView.playSound("./audio/hammerhead.mp3", 0.5);
@@ -100,9 +120,11 @@ const GameController = {
       GameView.playSound("./audio/hammerpipe.mp3", 0.5);
     }
   },
-  spawnEntity: function(type) {
+  spawnEntity: function (type) {
     if (GameModel.isGameOver) return;
-    let tile = document.getElementById(Math.floor(Math.random() * 16).toString());
+    let tile = document.getElementById(
+      Math.floor(Math.random() * 16).toString()
+    );
     if (tile.children.length === 0) {
       GameView.renderEntity(type, tile);
       GameView.playSound("./audio/pop.mp3", 0.2);
@@ -114,14 +136,15 @@ const GameController = {
       }, 2500 / GameModel.level);
     }
   },
-  startGameLoops: function() {
-    setInterval(() => this.spawnEntity("enemy"), 1500 / GameModel.level);
+  startGameLoops: function () {
+    setInterval(() => this.spawnEntity("enemy"), 1000 / GameModel.level);
     setInterval(() => this.spawnEntity("player"), 1800 / GameModel.level);
     setInterval(() => this.spawnEntity("enemy"), 2100 / GameModel.level);
-  }
+  },
 };
 
 window.onload = () => {
-  document.body.addEventListener("click", GameController.startGame, { once: true });
+  document.body.addEventListener("click", GameController.startGame, {
+    once: true,
+  });
 };
-
